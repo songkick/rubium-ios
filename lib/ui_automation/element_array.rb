44a7e945aecbb7798161edef381aa7453a8f72c4
Luke Redpath <luke@lukeredpath.co.uk>
2014-07-06 23:30:37 +0100

    Document ElementArray.

--- lib/ui_automation/element_array.rb
+++ lib/ui_automation/element_array.rb
@@ -6,12 +6,27 @@ module UIAutomation
   # This class implements #each and includes the Enumerable mixin, enabling you to use it like any other
   # native Ruby collection class.
   #
+  # All methods in thie class that return a UIAutomation::Element may return a specific sub-class of 
+  # UIAutomation::Element, as determined by the #element_klass attribute.
+  #
   # @see https://developer.apple.com/library/ios/documentation/ToolsLanguages/Reference/UIAElementArrayClassReference/
   class ElementArray < RemoteProxy
     include Enumerable
 
-    attr_reader :parent, :window, :element_klass
+    # The proxy for the parent element
+    # @note This returns the existing proxy that created this one; it does not use the UIAElement.parent() method
+    # @return [UIAutomation::Element]
+    attr_reader :parent
 
+    # The window that contains this element
+    # @return [UIAutomation::Window]
+    attr_reader :window
+    
+    # The element proxy class used for each element in the array.
+    # @api private
+    attr_reader :element_klass
+
+    # @api private
     def initialize(driver, remote_object, element_klass = UIAutomation::Element, parent_element = nil, window = nil)
       super driver, remote_object
       @element_klass = element_klass
@@ -22,45 +37,115 @@ module UIAutomation
     def inspect
       "<RemoteProxy(#{self.class.name}) type:#{element_proxy_for}: #{to_javascript}>"
     end
+    
+    # @!group Accessing Elements    
 
+    # Returns a proxy to the element at the given index.
+    # 
+    # @param [Integer] index
+    # @return [UIAutomation::Element] the element proxy
+    #
     def at_index(index)
       build_proxy element_klass, remote_object.object_for_subscript(index), [parent, window]
     end
 
+    # Returns a new ElementArray containing all elements with the given name.
+    # 
+    # @param [String] name
+    # @return [UIAutomation::ElementArray] a sub-set of the current element array
+    # @see UIAElementArray.withName()
+    #
     def with_name(name)
       element_array_proxy_for :withName, name
     end
 
+    # Returns a proxy to the first element with the given name.
+    # 
+    # @param [String] name
+    # @return [UIAutomation::Element] the element proxy
+    # @see UIAElementArray.firstWithName()
+    #
     def first_with_name(name)
       element_proxy_for :firstWithName, name
     end
 
-    def with_predicate(template, subtitutions)
+    # Returns a new ElementArray containing all elements matching the given predicate.
+    #
+    # Predicates use the Apple NSPredicate syntax.
+    #
+    # To avoid dealing with escaping and quoting of string values, you can use template substitution
+    # in your predicate string and pass in the values as a hash.
+    #
+    # @example Using predicate template substitution
+    #     array.with_predicate("name beginswith :value", :value => 'test')
+    # 
+    # @param [String] template a predicate template using NSPredicate syntax
+    # @param [Hash] substitution an optional hash of template substitutions
+    # @return [UIAutomation::ElementArray] a sub-set of the current element array
+    # @see UIAElementArray.withPredicate()
+    # @see https://developer.apple.com/library/ios/documentation/Cocoa/Conceptual/Predicates/Articles/pSyntax.html
+    #
+    def with_predicate(template, subtitutions = {})
       element_array_proxy_for :withPredicate, UIAutomation::PredicateString.new(template, subtitutions).to_s
     end
 
-    def first_with_predicate(template, subtitutions)
+    # Returns a proxy to the first element matching the specified predicate.
+    #
+    # Predicates use the Apple NSPredicate syntax (see #with_predicate for more information).
+    #
+    # @param [String] template a predicate template using NSPredicate syntax
+    # @param [Hash] substitution an optional hash of template substitutions
+    # @return [UIAutomation::Element] the element proxy
+    # @see #with_predicate
+    # @see UIAElementArray.firstWithPredicate()
+    #
+    def first_with_predicate(template, subtitutions = {})
       element_proxy_for :firstWithPredicate, UIAutomation::PredicateString.new(template, subtitutions).to_s
     end
 
+    # Returns a new ElementArray containing all elements with the given key/value pair.
+    # 
+    # @param [String] key 
+    # @param [Object] value
+    # @return [UIAutomation::ElementArray] a sub-set of the current element array
+    # @see UIAElementArray.withValueForKey()
+    #
     def with_value_for_key(key, value)
       element_array_proxy_for :withValueForKey, value, key
     end
 
+    # Returns a new ElementArray containing all elements with the given value.
+    # 
+    # @param [Object] value
+    # @return [UIAutomation::ElementArray] a sub-set of the current element array
+    #
     def with_value(value)
       with_value_for_key(:value, value)
     end
 
+    # Returns a proxy to the first element with the given value.
+    # 
+    # @param [Object] value the element value
+    # @return [UIAutomation::Element] the element proxy or nil if no elements found
+    # @see UIAElementArray.firstWithName()
+    #
     def first_with_value(value)
       collection = with_value(value)
       collection.any? ? collection.first : nil
     end
-
-    def length
-      fetch(:length)
-    end
-    alias :count :length
-
+    
+    # Returns a remote proxy to a specific element within the collection by index or name.
+    #
+    # You can use square-bracked syntax with either an Integer or String key to perform lookup
+    # by index or name respectively.
+    #
+    # Calling with an Integer is equivalent to calling #at_index. Calling with a String is
+    # equivalent to calling #first_with_name.
+    # 
+    # @param [String or Integer] index_or_name the index or name of the attribute
+    # @return [UIAutomation::Element] the matching element
+    # @see UIAElementArray.withName()
+    #
     def [](index_or_name)
       if index_or_name.is_a?(Integer)
         at_index(index_or_name)
@@ -68,7 +153,24 @@ module UIAutomation
         first_with_name(index_or_name)
       end
     end
+    
+    # @!endgroup
 
+    # Returns the number of items in the array
+    # @return [Integer]
+    #
+    def length
+      fetch(:length)
+    end
+    alias :count :length
+
+    # Yields a proxy to each element in the ElementArray if a block is given.
+    #
+    # If no block is given, this method will return an Enumerator.
+    #
+    # @yieldparam [UIAutomation::Element] element if block given
+    # @return [Enumerator] if no block is given
+    #
     def each(&block)
       if block_given?
         (0...length).each do |index|
@@ -98,6 +200,7 @@ module UIAutomation
     end
   end
 
+  # @private
   class PredicateString
     def initialize(template, substitutions = {})
       @template = template
