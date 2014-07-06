b869120d8a5e6df9b0e03afa0dacd9e700850846
Luke Redpath <luke@lukeredpath.co.uk>
2014-07-06 23:36:53 +0100

    Document Keyboard.

--- lib/ui_automation/keyboard.rb
+++ lib/ui_automation/keyboard.rb
@@ -4,18 +4,37 @@ module UIAutomation
   # @see https://developer.apple.com/library/ios/documentation/ToolsLanguages/Reference/UIAKeyboardClassReference/
   #
   class Keyboard < UIAutomation::Element
+    ### @!group Element Collections
+
+    # The keyboard's keys
     has_element_array :keys
+
+    ### @!endgroup
     
-    def type(string)
-      perform :typeString, string
-    end
-    
+    ### @!group Elements
+
+    # @return [UIAutomation::Element] the keyboard's done button
     def done_button
       buttons['Done']
     end
     
+    # @return [UIAutomation::Element] the keyboard's return button
     def return_button
       buttons['Return']
     end
+
+    ### @!endgroup
+   
+    ### @!group Actions
+
+    # Types the given string by simulating key presses.
+    #
+    # @param [String] string the string to type
+    #
+    def type(string)
+      perform :typeString, string
+    end
+
+    ### @!endgroup
   end
 end
