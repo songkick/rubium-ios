004078c23a454fb924fe3bb82c2755f15500b5b3
Luke Redpath <luke@lukeredpath.co.uk>
2014-07-08 17:32:16 +0100

    Revert "Temporarily disable CodeClimate test reporting to see if thats "
    This reverts commit 05df6ebadbcb51e959f5fad3f015667a3b189524.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,8 +4,8 @@ unless ENV['TM_FILEPATH']
   require 'simplecov'
   SimpleCov.start unless ENV['skip_coverage']
 
-  # require "codeclimate-test-reporter"
-  # CodeClimate::TestReporter.start if ENV['CODECLIMATE_REPO_TOKEN']
+  require "codeclimate-test-reporter"
+  CodeClimate::TestReporter.start if ENV['CODECLIMATE_REPO_TOKEN']
   
   require 'coveralls'
   Coveralls.wear!
