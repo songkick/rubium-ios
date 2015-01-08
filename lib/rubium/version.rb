ae102a5fd23129f5bf29ed5a1794bb2b9df3740c
Aaron Randall <aaron@songkick.com>
2015-01-08 16:08:44 +0000

    Bump gem version.

--- lib/rubium/version.rb
+++ lib/rubium/version.rb
@@ -2,15 +2,15 @@ module Rubium
   module Version
     MAJOR = 1
     MINOR = 0
-    TINY  = 0
+    TINY  = 1
 
     def self.to_s
       version = [MAJOR, MINOR, TINY].join(".")
       prerelease ? "#{version}.pre" : version
     end
-    
+
     def self.prerelease
-      true
+      false
     end
   end
 end
